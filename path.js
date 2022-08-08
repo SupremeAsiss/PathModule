8d3fd12dd137d4c65961db61d2c1ccc95ab7fd0c
Ashish Tamang <ghishingashish22774@gmail.com>
2022-08-08 14:48:17 +0545

    Path Module of Node JS

---
+++ path.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+//console.log(path.dirname('C:\Users\DELL\Desktop\NodePC\pathModule\path.js'));
+
+//console.log(path.extname('C:\Users\DELL\Desktop\NodePC\pathModule\path.js'));
+
+console.log(path.basename('C:\Users\DELL\Desktop\NodePC\pathModule\path.js'));
+console.log(path.parse('C:\Users\DELL\Desktop\NodePC\pathModule\path.js'));
